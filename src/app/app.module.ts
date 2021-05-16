65f61bdce77b769c3abd392d0abf2d562d578059
cheruden <cheruden25@gmail.com>
2021-05-16 14:02:45 +0300

    add imports from quotes folder to my pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms"
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './quote-form/quote-form.component';
+import { DetailsComponent } from './details/details.component';
+import { QuoteComponent } from './quote/quote.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    DetailsComponent,
+    QuoteComponent,
+    // GoalFormComponent,
+    // QuiteFormComponent,
+    // QuoteFormComponent,
+    // HighlightDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
